fd2ceb5ba3f397ba3e4f796f4e410641fbb1b182
Oliver Braun <ob@obraun.net>
2017-07-26 13:11:44 +0200

    fixed copy issue

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const htmlStandards = require('reshape-standard')
-//  const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
 const env = process.env.NODE_ENV
 
 module.exports = {
-  // devtool: 'source-map',
   matchers: { html: '*(**/)*.sgr' },
   ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
   reshape: htmlStandards({
     locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
     minify: env === 'production'
-  })
-//  babel: jsStandards()
+  }),
+  vendor: ['assets/js/**', 'assets/cubeportfolio/js/**']
 }
