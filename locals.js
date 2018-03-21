2708d985bf276133e015a7f901368232eaf804ca
Oliver Braun <ob@cs.hm.edu>
2018-03-21 13:33:14 +0100

    split lecture json into different files

--- locals.js
+++ locals.js
@@ -1,15 +1,31 @@
-const algdati = require('./api/lectures/algdati.json')
-const algdatii = require('./api/lectures/algdatii.json')
-const compiler = require('./api/lectures/compiler.json')
-const fun = require('./api/lectures/fun.json')
-const sa = require('./api/lectures/sa.json')
-const seiib = require('./api/lectures/seiib.json')
-const seiiib = require('./api/lectures/seiiib.json')
-const swengiib = require('./api/lectures/swengiib.json')
-const swengiiib = require('./api/lectures/swengiiib.json')
-const vss = require('./api/lectures/vss.json')
-const webtech = require('./api/lectures/webtech.json')
-const webtechFK12 = require('./api/lectures/webtechFK12.json')
+const initLecture = (lecturename) => {
+  const lecture = require(`./api/lectures/${lecturename}/base.json`)
+  if (lecture.maincontent !== null) {
+    if (lecture.maincontent.lecture !== null) {
+      lecture.maincontent.lecture.lectures =
+        require(`./api/lectures/${lecturename}/lectures.json`)
+    }
+    if (lecture.maincontent.lab !== null) {
+      lecture.maincontent.lab.labs =
+        require(`./api/lectures/${lecturename}/labs.json`)
+    }
+  }
+  return lecture
+}
+
+const algdati = initLecture('algdati') // require('./api/lectures/algdati.json')
+const algdatii = initLecture('algdatii') // require('./api/lectures/algdatii.json')
+const compiler = initLecture('compiler') // require('./api/lectures/compiler.json')
+const fun = initLecture('fun') // require('./api/lectures/fun.json')
+const sa = initLecture('sa') // require('./api/lectures/sa.json')
+const seiib = initLecture('seiib') // require('./api/lectures/seiib.json')
+const seiiib = initLecture('seiiib') // require('./api/lectures/seiiib.json')
+const swengiib = initLecture('swengiib') // require('./api/lectures/swengiib.json')
+const swengiiib = initLecture('swengiiib') // require('./api/lectures/swengiiib.json')
+const vss = initLecture('vss') // require('./api/lectures/vss.json')
+const webtech = initLecture('webtech') // require('./api/lectures/webtech.json')
+const webtechFK12 = initLecture('webtechFK12') // require('./api/lectures/webtechFK12.json')
+
 const base = require('./api/base.json')
 
 const lectures = [
@@ -37,8 +53,7 @@ exports.currentSemester = {
     seiiib,
     vss
   ],
-  semesters: [
-    {
+  semesters: [{
       filter: '.ss18',
       label: 'SS 18'
     },
