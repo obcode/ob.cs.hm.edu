c2cafd68fcc13fcf1c8d0191ea64b0fec5d2673d
Oliver Braun <ob@cs.hm.edu>
2018-09-23 10:43:35 +0200

    more news

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,23 @@ const fetchnews = (lecture) => {
     $.getJSON(url, news => {
         if (news.length > 0) {
             let newsContent = '<h3>Aktuelles:</h3>'
-            for (let entry of news) {
+            for (let index in news) {
+                if (index >= 3) break
+                const entry = news[index]
                 newsContent +=
                     `<div class="row">
                         <div class="col-md-3">${entry.date}</div>
                         <div class="col-md-9">${entry.subject}</div>
                     </div>`
+            }
+            if (news.length > 3) {
+                newsContent +=
+                    `<div class="row">
+                        <div class="col-md-3">
+                            <a href="/public/news/${lecture}.json">ältere Infos</a>
+                        </div>
+                        <div class="col-md-9"></div>
+                    </div>`                
             }
             newsContent += '<hr>'
             $('#news').html(newsContent)
