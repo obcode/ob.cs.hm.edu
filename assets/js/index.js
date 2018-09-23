802010ba463a193ed62361ecf49ae43cb29cd3c8
Oliver Braun <ob@cs.hm.edu>
2018-09-23 08:25:26 +0200

    add support for news

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,17 @@
-// not needed
+const fetchnews = (lecture) => {
+    const url = `/public/news/${lecture}.json`
+    $.getJSON(url, news => {
+        if (news.length > 0) {
+            let newsContent = '<h3>Aktuelles:</h3>'
+            for (let entry of news) {
+                newsContent +=
+                    `<div class="row">
+                        <div class="col-md-3">${entry.date}</div>
+                        <div class="col-md-9">${entry.subject}</div>
+                    </div>`
+            }
+            newsContent += '<hr>'
+            $('#news').html(newsContent)
+        }
+    })
+}
